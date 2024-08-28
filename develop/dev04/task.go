a0ca4a870c064e9c5b5372ef4dcc5fe6585901f0
Katarzhin Mikhail <maikl.1997@mail.ru>
2024-08-28 23:41:28 +0400

    [feat] dev04 tasks complete

--- develop/dev04/task.go
+++ develop/dev04/task.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"slices"
+	"strings"
+)
+
 /*
 === Поиск анаграмм по словарю ===
 
@@ -19,6 +25,54 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+func findAnagramsFromSlice(words []string) map[string][]string {
+	anagrams := make(map[string][]string)
 
+	// checkedWords для хранения уже считанных слов с целью поддержания их уникальности
+	checkedWords := make(map[string]struct{})
+
+	for _, word := range words {
+		// Приводим слово к нижнему регистру
+		lowerWord := strings.ToLower(word)
+
+		if _, ok := checkedWords[lowerWord]; !ok {
+			checkedWords[lowerWord] = struct{}{}
+
+			arr := []byte(lowerWord)
+			slices.SortFunc(arr, func(a, b byte) int {
+				if a < b {
+					return -1
+				} else if a > b {
+					return 1
+				}
+				return 0
+			})
+
+			anagrams[string(arr)] = append(anagrams[string(arr)], lowerWord)
+		}
+	}
+
+	// Генерирация итоговой мапы, соответствующуй заданию
+	result := make(map[string][]string)
+
+	for _, group := range anagrams {
+		if len(group) > 1 {
+			// Сортировка группы
+			slices.Sort(group)
+			// Закрепление первого слова из отсортированной группы ключом группы в словваре
+			result[group[0]] = group
+		}
+	}
+
+	return result
+}
+
+func main() {
+	words := []string{"пятак", "столик", "пяткА", "тяпка", "тяпка", "тЯпка", "листок", "слиток", "слово", "волос", "ослов", "зола"}
+
+	anagramGroups := findAnagramsFromSlice(words)
+
+	for key, group := range anagramGroups {
+		fmt.Println(key, ":", group)
+	}
 }
